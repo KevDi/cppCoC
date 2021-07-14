b31a53be3ec8412ad791b6ef51a96405b6c96b0a
Kevin <kevin@mjkd.de>
2021-07-14 23:09:29 +0200

    Refactored Tests

--- models/player/tests/player_test.cpp
+++ models/player/tests/player_test.cpp
@@ -6,18 +6,23 @@
 using namespace testing;
 using namespace jd::coc;
 
+static const std::string PLAYER_JSON_PATH = "resource/player.json";
+static const std::string ALTERNATIVE_PLAYER_JSON_PATH = "resource/player_alternative.json";
 class Player : public Test {
    public:
     void SetUp() override {
-        std::ifstream ifile{"resource/player.json"};
+        player_.load_data(load_file(PLAYER_JSON_PATH));
+    }
+
+    std::string load_file(const std::string& filename) {
+        std::ifstream ifile{filename};
         std::string example;
         ifile.seekg(0, std::ios::end);
         example.reserve(ifile.tellg());
         ifile.seekg(0, std::ios::beg);
 
         example.assign((std::istreambuf_iterator<char>(ifile)), std::istreambuf_iterator<char>());
-
-        player_.load_data(example);
+        return example;
     }
 
     model::Player player_{};
@@ -44,15 +49,7 @@ TEST_F(Player, LoadDataSetsTownhallWeaponLevel) {
 }
 
 TEST_F(Player, LoadDataSetsTownhallWeaponLevelToZeroIfNotFound) {
-    std::ifstream ifile{"resource/player_alternative.json"};
-    std::string example;
-    ifile.seekg(0, std::ios::end);
-    example.reserve(ifile.tellg());
-    ifile.seekg(0, std::ios::beg);
-
-    example.assign((std::istreambuf_iterator<char>(ifile)), std::istreambuf_iterator<char>());
-
-    player_.load_data(example);
+    player_.load_data(load_file(ALTERNATIVE_PLAYER_JSON_PATH));
 
     ASSERT_THAT(player_.townhall_weapon_level(), Eq(0));
 }
